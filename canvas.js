d67c564f4220a28fc2d2bf1b5e32bc02c02babf5
npv2k1 <npv2k1@gmail.com>
2020-12-20 21:15:35 +0700

    Update comment code

--- canvas.js
+++ canvas.js
@@ -1,122 +1,64 @@
+// Hiển thị log
 function logg(e) {
   console.log("Position: ", e.clientX, ":", e.clientY);
 }
-var arr = [];
+
+
+
+/**
+ * TODO Chuyển mã màu RGB sang hex
+ * @param {Decimal} c 
+ */
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+/**
+ * TODO Khởi tạo
+ */
+var arr = []; //Tạo mảng để nhớ những điểm đã vẽ
 window.addEventListener("load", () => {
-  const canvas = document.getElementById("canvas");
-  const ctx = canvas.getContext("2d");
+  const canvas = document.getElementById("canvas"); // Xác định canvas
+  const ctx = canvas.getContext("2d"); // tạo context 2d để thao tác
+  // resize canvas
   canvas.height = 500;
   canvas.width = 1000;
-  //   ctx.fillRect(0, 0, 50, 50);
-  //   console.log("hello");
 
-  //   ctx.beginPath();
-  //   ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
-  //   ctx.moveTo(110, 75);
-  //   ctx.arc(75, 75, 35, 0, Math.PI, false); // Mouth (clockwise)
-  //   ctx.moveTo(65, 65);
-  //   ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // Left eye
-  //   ctx.moveTo(95, 65);
-  //   ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // Right eye
-  //   ctx.stroke();
-  // Cubic curves example
+  var preX, preY; // Lưu lại tọa độ điểm trước đó
 
-  //   ctx.beginPath();
-  //   ctx.moveTo(75, 40);
-  //   ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
-  //   ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
-  //   ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
-  //   ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
-  //   ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
-  //   ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
-  //   ctx.fill();
-
-  function componentToHex(c) {
-    var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
-  }
-
-  function rgbToHex(r, g, b) {
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-  }
-  var r = 50,
-    g = 50,
-    b = 50;
-  var preX, preY;
   let p = false;
-  ctx.lineWidth = 10;
-  ctx.lineCap = "round";
 
+  ctx.lineWidth = 10; // Bề rộng đường vẽ
+  ctx.lineCap = "round"; // Loại đường vẽ
+
+  /**
+   * TODO hàm vẽ.
+   * ! e: chứa toàn bộ thông tin khi sự kiện kích hoạt (tọa độ chuôt, ...)
+   * @param {event} e
+   * ! canvas.offsetLeft : trả về vị trí canvas với đối tượng cha
+   * ! trả về tọa độ chuột e.clientX
+   */
   function draw(e) {
     if (!p) return;
 
     ctx.beginPath();
-    // logg(e);
-    ctx.moveTo(preX, preY);
-    // ctx.moveTo(0, 0);
-    ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    ctx.moveTo(preX, preY); // di chuyển đến tọa tộ (...,...)
+    ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop); // vẽ 1 đường tới toạn độ (..., ...). nếu không có moveTo thì nó sẽ vẽ thành những chấm
     preX = e.clientX - canvas.offsetLeft;
     preY = e.clientY - canvas.offsetTop;
-   
     arr.push([e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop]);
-    // console.log(r,g,b)
-    // if(r<=255){
-    //     ctx.strokeStyle = rgbToHex(r, g, b);
-    //     r++;
-    // }
-    // else{
-    //     if(g<=255){
-    //          ctx.strokeStyle = rgbToHex(r, g, b);
-    //          g+=30;
-    //          r=0;
-    //     }
-    //     else{
-    //         if(b<=255){
-    //             ctx.strokeStyle = rgbToHex(r, g, b);
-    //             b++;
-    //             g=0;
-    //             r=0;
-    //         }
-    //     }
-    // }
-
-    //ctx.lineTo(e.clientX, e.clientY);
     ctx.stroke();
     ctx.closePath();
-    
   }
-
-  function delay(delayInms) {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(2);
-      }, delayInms);
-    });
-  }
-  async function autoDraw(arr) {
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-
-    // arr.forEach(i=>{
-    //     //console.log(i)
-
-    //     ctx.lineTo(i[0], i[1]);
-
-    // })
-
-    for (var i = 1; i < arr.length; i++) {
-      ctx.beginPath();
-      ctx.lineTo(arr[i][0], arr[i][1]);
-
-      ctx.stroke();
-      let delayres = await delay(10);
-    }
-  }
-
   canvas.addEventListener("mousedown", (e) => {
     p = true;
     ctx.beginPath();
-    ctx.strokeStyle="black"
+    ctx.strokeStyle = "black"; // đặt màu cho đường vẽ
     preX = e.clientX - canvas.offsetLeft;
     preY = e.clientY - canvas.offsetLeft;
     ctx.moveTo(e.clientX, e.clientY);
@@ -129,25 +71,27 @@ window.addEventListener("load", () => {
   canvas.addEventListener("mouseup", () => {
     p = false;
     console.log(arr);
-   
-
-    //tx.clearRect(0, 0, canvas.width, canvas.height);
-    //autoDraw(arr);
-    //arr = [];
   });
   canvas.addEventListener("mousemove", (e) => {
     draw(e);
   });
 });
-
+/**
+ * TODO xóa canvas
+ */
 function clearCanvas() {
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
+/**
+ * TODO vẽ lại theo mảng arr
+ */
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+
 function delay(delayInms) {
+  // delay function
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(2);
@@ -166,16 +110,9 @@ async function autoDraw(arr) {
 
       ctx.stroke();
       let delayres = await delay(1);
+      
     }
-    // for (var i = 1; i < 1000; i++) {
-    //   ctx.beginPath();
-    //   ctx.lineTo(i+10, i+50);
-
-    //   ctx.stroke();
-    //   let delayres = await delay(1);
-    // }
 }
-
 function drawAgain() {
   autoDraw(arr);
   arr = [];
